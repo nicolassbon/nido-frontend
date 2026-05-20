1a656136f87ef1e93a728a2a2254cdc43b665556
giuliannadr <giulianadirocco1@gmail.com>
2026-05-20 15:39:13 -0300

    fix(tests): correct relative import paths for appConfig

--- src/app/features/alacena/alacena/alacena.spec.ts
+++ src/app/features/alacena/alacena/alacena.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { appConfig } from '../../../../app.config';
+import { appConfig } from '../../../app.config';
 import { Alacena } from './alacena';
 
 describe('Alacena', () => {
